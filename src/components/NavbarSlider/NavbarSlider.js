7a37b97d68441f26c2226f2bda211690969476d4
Niharika Upadhyayula <niharika.u03@gmail.com>
2023-05-11 14:22:54 -0400

    updated Landing Page Visuals

--- src/components/NavbarSlider/NavbarSlider.js
+++ src/components/NavbarSlider/NavbarSlider.js
@@ -6,9 +6,6 @@ import Wrapper from "../UI/Wrapper/Wrapper";
 import Title from "../UI/Title/Title";
 import Subtitle from "../UI/Subtitle/Subtitle";
 
-
-
-
 const NavbarSlider = () => {
   return (
     <HeroSlider
@@ -18,8 +15,8 @@ const NavbarSlider = () => {
       onBeforeChange={(previousSlide, nextSlide) =>
         console.log("onBeforeChange", previousSlide, nextSlide)
       }
-      onChange={nextSlide => console.log("onChange", nextSlide)}
-      onAfterChange={nextSlide => console.log("onAfterChange", nextSlide)}
+      onChange={(nextSlide) => console.log("onChange", nextSlide)}
+      onAfterChange={(nextSlide) => console.log("onAfterChange", nextSlide)}
       style={{
         backgroundColor: "#000"
       }}
@@ -33,10 +30,11 @@ const NavbarSlider = () => {
       }}
     >
       <Overlay>
-      <NavBar/>
+        <NavBar />
         <Wrapper>
-          <Title>Navbar Slider</Title>
-          <Subtitle>Cool stuff.</Subtitle>
+          <Title>Hotel Management System</Title>
+          <Subtitle>Niharika Upadhyayula</Subtitle>
+          <Subtitle>Meghana Purijala</Subtitle>
         </Wrapper>
       </Overlay>
 
@@ -45,16 +43,19 @@ const NavbarSlider = () => {
         navDescription="Kyoto - Japan"
         background={{
           backgroundColor: "#6D9B98",
-          backgroundImage: "url('https://i.imgur.com/xw5Abku.jpg')"
+          backgroundImage:
+            "url('https://media.cntraveler.com/photos/57dac251248d564c67def553/16:9/w_2560%2Cc_limit/Sunset-AnantaraUluwataBaliResort-Bali-CRHotel.jpg')",
+          backgroundSize: "cover" // Added property
         }}
       />
 
       <Slide
         shouldRenderMask
-        navDescription="Tenryu-ji Temple - KyÅto-shi - Japan"
+        navDescription="Tenryu-ji Temple - Kyōto-shi - Japan"
         background={{
           backgroundColor: "#8A8A8A",
-          backgroundImage: "url('https://i.imgur.com/idjXzVQ.jpg')"
+          backgroundImage: "url('https://wallpaperaccess.com/full/6423030.jpg')",
+          backgroundSize: "cover" // Added property
         }}
       />
 
@@ -63,7 +64,8 @@ const NavbarSlider = () => {
         navDescription="Hakone - Japan"
         background={{
           backgroundColor: "#EA2329",
-          backgroundImage: "url('https://i.imgur.com/8DYumaY.jpg')"
+          backgroundImage: "url('https://www.jetsetter.com//uploads/sites/7/2018/04/lG1HB2u-.jpeg')",
+          backgroundSize: "cover" // Added property
         }}
       />
 
@@ -72,7 +74,8 @@ const NavbarSlider = () => {
         navDescription="Byodo-In Temple - Kaneohe - United States"
         background={{
           backgroundColor: "#2D7791",
-          backgroundImage: "url('https://i.imgur.com/8IuucQZ.jpg')"
+          backgroundImage: "url('https://lajolla.com/wp-content/uploads/2018/03/13963023_975015099273904_3571657806461137454_o.jpg')",
+          backgroundSize: "cover" // Added property
         }}
       />
 
